14237d77d2475f9e8f6e6236a766cd26a4acd840
JulieBoberg <43385336+JulieBoberg@users.noreply.github.com>
2020-02-04 07:11:35 -0800

    Spinner, dashboard setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React, { Fragment, useEffect } from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "../src/components/layout/Landing";
 import Navbar from "../src/components/layout/Navbar";
+import Alert from "./components/layout/Alert";
 import Login from "../src/components/auth/Login";
 import Register from "../src/components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
-import Alert from "../src/components/layout/Alert";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import { loadUser } from "./actions/auth";
-import { Provider } from "react-redux";
 import setAuthToken from "../src/utils/setAuthToken";
 import store from "./store";
-import PrivateRoute from "./components/routing/PrivateRoute";
+
 import "./App.css";
 
 if (localStorage.token) {
