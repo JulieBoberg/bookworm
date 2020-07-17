40928dcd4040dff15f2c41527e93c4f64cca4464
JulieBoberg <43385336+JulieBoberg@users.noreply.github.com>
2020-07-17 10:04:15 -0700

    Minor changes to profile

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -5,6 +5,7 @@ const ProfileAbout = ({
   profile: {
     bio,
     identities,
+    keyWords,
     user: { name },
   },
 }) => {
@@ -22,6 +23,18 @@ const ProfileAbout = ({
         {identities.map((identity, index) => (
           <div key={index}>
             <span class='badge badge-pill badge-danger'>{identity}</span>
+          </div>
+        ))}
+      </div>
+
+      {/*  */}
+      <div class='line'></div>
+
+      <h2 class='text-primary'>My keywords</h2>
+      <div class='skills'>
+        {keyWords.map((words, index) => (
+          <div key={index}>
+            <span class='badge badge-pill badge-danger'>{words}</span>
           </div>
         ))}
       </div>
--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -6,6 +6,7 @@ const ProfileTop = ({
     publicName,
     brandName,
     locationState,
+    locationCity,
     website,
     social,
     user: { avatar },
@@ -13,42 +14,55 @@ const ProfileTop = ({
   },
 }) => {
   return (
-    <div className='profile-top bg-primary p-2'>
+    <div className='profile-top'>
       <img className='round-img my-1' src={avatar} alt='' />
-      {/* <h1 className='large'>{publicName}</h1> */}
-      <p className='lead'>
-        {publicName} {brandName && <span> of {brandName}</span>}
-      </p>
-      <p>{locationState && <span>{locationState}</span>}</p>
-      <div className='icons my-1'>
-        {website && (
-          <a href={website} target='_blank' rel='noopener noreferrer'>
-            <i className='fas fa-globe fa-2x'></i>
-          </a>
-        )}
+      <div className='bg-primary'>
+        <p className='lead'>
+          {publicName}{" "}
+          {brandName && (
+            <span>
+              <i> of {brandName}</i>
+            </span>
+          )}
+        </p>
+        <p>
+          {locationCity && <span> {locationCity}</span>}
+          {locationState && <span>, {locationState}</span>}
+        </p>
+        <div className='icons my-1'>
+          {website && (
+            <a href={website} target='_blank' rel='noopener noreferrer'>
+              <i className='fas fa-globe fa-2x'></i>
+            </a>
+          )}
 
-        {social && social.twitter && (
-          <a href={social.twitter} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-twitter fa-2x'></i>
-          </a>
-        )}
+          {social && social.twitter && (
+            <a href={social.twitter} target='_blank' rel='noopener noreferrer'>
+              <i className='fab fa-twitter fa-2x'></i>
+            </a>
+          )}
 
-        {social && social.facebook && (
-          <a href={social.facebook} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-facebook fa-2x'></i>
-          </a>
-        )}
+          {social && social.facebook && (
+            <a href={social.facebook} target='_blank' rel='noopener noreferrer'>
+              <i className='fab fa-facebook fa-2x'></i>
+            </a>
+          )}
 
-        {social && social.youtube && (
-          <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-youtube fa-2x'></i>
-          </a>
-        )}
-        {social && social.instagram && (
-          <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-instagram fa-2x'></i>
-          </a>
-        )}
+          {social && social.youtube && (
+            <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
+              <i className='fab fa-youtube fa-2x'></i>
+            </a>
+          )}
+          {social && social.instagram && (
+            <a
+              href={social.instagram}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='fab fa-instagram fa-2x'></i>
+            </a>
+          )}
+        </div>
       </div>
     </div>
   );
