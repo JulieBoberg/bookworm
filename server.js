5293c3b7c8f9c156208b153a90d20da886635ba9
JulieBoberg <43385336+JulieBoberg@users.noreply.github.com>
2020-09-18 10:14:26 -0700

    getting into production

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path");
 const connectDB = require("./config/db");
 
 const app = express();
@@ -8,12 +8,23 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
+//app.get("/", (req, res) => res.send("API Running"));
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+//production
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
